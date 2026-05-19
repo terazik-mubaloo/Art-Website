39f651af78f73cf3351857557916242173c62d80
terazik-mubaloo <78199108+terazik-mubaloo@users.noreply.github.com>
2026-05-19 03:30:45 +0100

    v2026.05.18.1
    - Added tma cafe 3d model.
    - Added blockbench logo.
    - Shrunk program logo files.
    - Added duck walk cycle to home page.
    - Overhauled animation stash.
    - Added art posts.

---
+++ js/burger-menu.js
@@ -0,0 +1,8 @@
+function myFunction() {
+    var x = document.getElementById("burger-links");
+    if (x.style.display === "block") {
+        x.style.display = "none";
+    } else {
+        x.style.display = "block";
+    }
+}
